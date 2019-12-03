d47a117e876e7ee80f1ec2998338acef398fed24
MannyMonocle <eetumadu@gmail.com>
2019-12-02 20:05:14 -0500

    now I need to refactor prices to pricepoints

--- src/main/java/ItemList.java
+++ src/main/java/ItemList.java
@@ -3,12 +3,29 @@ import java.util.HashMap;
 
 public class ItemList {
     ArrayList<Item> ogItems;
-   ArrayList<String> uniqueNames = new ArrayList<String>();
+   HashMap<String,ArrayList<String>> uniquesMap;
 
 
     public ItemList(ArrayList<Item> ogItems) {
         this.ogItems = ogItems;
+        this.uniquesMap = getUniqueNames();
+        addAllOGItemPrices();
     }
 
+    public HashMap<String, ArrayList<String>> getUniqueNames(){
+        HashMap<String, ArrayList<String>> names = new HashMap<String, ArrayList<String>>();
+        for(Item obj : ogItems){names.put(obj.getName(),new ArrayList<String>());}
+        return names;
+    }
+
+    public void addPricesToUniquesMap(Item item){
+        if(!uniquesMap.containsValue(item.getPrice())){
+            uniquesMap.get(item.getName()).add(item.getPrice());
+        }
+    }
+
+    public void addAllOGItemPrices(){
+        for(Item item : ogItems){addPricesToUniquesMap(item);}
+    }
 
 }
